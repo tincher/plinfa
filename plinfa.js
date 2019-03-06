cbe8713d3f965ef42a577cdbc616936e15f2e48d
Joel Ewig <joelewig@Joels-MacBook-Pro.local>
2019-03-07 00:24:47 +0100

    refactoring and fixing, now the storage and removal works like a charm

--- plinfa.js
+++ plinfa.js
@@ -1,25 +1,23 @@
-// get the current blacklits
-const blackList = getStorage();
-var all_videos = document.getElementsByTagName("ytd-item-section-renderer");
-var all_titles = document.getElementsByClassName("title-and-badge");
-
 // create a consistent db item
-function createBlacklistItem(blackWord) {
-    return {"value": blackWord.toLowerCase()};
+function createBlacklistItem(blackWord, blacklistObject) {
+    blacklistObject.value = (blacklistObject.value != undefined)?
+        blacklistObject.value.concat(blackWord): [blackWord];
+    // blacklistObject.value = [blackWord].concat(blacklistObject.value)
+    return blacklistObject;
 }
 
 // store the given word to the db
-function addItemToStorage(blackWord) {
-    browser.storage.local.set(createBlacklistItem(blackWord))
+function addItemToStorage(blackWord, blacklistObject) {
+    browser.storage.local.set(createBlacklistItem(blackWord, blacklistObject))
         .then(() => console.log("success"));
 }
 
 // collect the titles and return them in the right format
-function fetchTitles() {
+function fetchTitles(videos, titles) {
     var result = [];
-    for (var i = 0; i < all_videos.length; i++){
-            video = all_videos.item(i);
-            title = all_titles.item(i).children.namedItem("video-title").text;
+    for (var i = 0; i < videos.length; i++){
+            video = videos.item(i);
+            title = titles.item(i).children.namedItem("video-title").text;
             result.push(title.toLowerCase().split(/\s|\\n/g).filter((x)=>x!="").join(" "));
     }
     return result;
@@ -27,36 +25,48 @@ function fetchTitles() {
 
 // get the current storage
 function getStorage() {
-    let result = {};
-    browser.storage.local.get().then((blackList) => {
-        result = blackList;
+    return browser.storage.local.get().then((result) => {
+        return result;
     }).catch((e) => console.log("Error while trying to get blacklist from storage"));
-    return result;
 }
 
 // filter the sub elements for the
-function filterSubsByBlacklist(elements){
-    elements.forEach((element) =>{
-        if (isAnyFromArrayInString(blackList, element)){
-            removeSubFromSite(element);
+function filterSubsByBlacklist(videos, titles, blacklist){
+    titles.forEach((element) => {
+        if (isAnyFromArrayInString(blacklist, element)){
+            removeSubFromSite(element, videos, titles);
         }
     });
 }
 
 // remove the subscription video from the site
-function removeSubFromSite(videoTitle) {
-    videoIndex = fetchTitles().indexOf(videoTitle);
-    all_videos[videoIndex].parentNode.removeChild(all_videos[videoIndex]);
+function removeSubFromSite(videoTitle, videos, titles) {
+    videoIndex = titles.indexOf(videoTitle);
+    videos[videoIndex].parentNode.removeChild(videos[videoIndex]);
     console.log("Index: " + videoIndex);
 }
 
 // checks if any of the keywords is in the provided title
 function isAnyFromArrayInString(words, title) {
     let result = false;
-    words.forEach((word) => {
+    words.value.forEach((word) => {
         if (title.includes(word)) {
             result = true;
         }
     });
     return result;
 }
+
+// main function
+function main() {
+    getStorage().then((blacklistObject) => {
+        var all_videos = document.getElementsByTagName("ytd-item-section-renderer");
+        var all_titles = document.getElementsByClassName("title-and-badge");
+        var titles = fetchTitles(all_videos, all_titles);
+        // addItemToStorage("wired", blacklistObject);
+        // addItemToStorage("nhl", blacklistObject);
+        filterSubsByBlacklist(all_videos, titles, blacklistObject);
+    })
+}
+
+main();
